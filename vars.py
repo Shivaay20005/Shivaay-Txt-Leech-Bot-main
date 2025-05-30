03cd6abc8684ca58d21a0341666923617ef980fa
Shivaay <shivaay20005@gmail.com>
2025-05-31 02:26:12 +0530

    Initial Heroku-ready commit

--- vars.py
+++ vars.py
@@ -1,5 +1,6 @@
-from os import environ
+import os
+import environ
 
-API_ID = int(environ.get("API_ID", "29473197"))
-API_HASH = environ.get("API_HASH", "182c589126cae7e6128a63a5c0fd40d8")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7843459688:AAGCeQjdFqWGFrucHPRuV4XD-Uuj60WGoBU")
+API_ID = int(os.environ[29473197])
+API_HASH = os.environ["182c589126cae7e6128a63a5c0fd40d8"]
+BOT_TOKEN = os.environ["843459688:AAGCeQjdFqWGFrucHPRuV4XD-Uuj60WGoBU"]
