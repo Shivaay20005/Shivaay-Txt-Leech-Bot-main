69a9826b7076da619cf1e963e65b9570f98ed113
Shivaay <shivaay20005@gmail.com>
2025-05-31 07:41:10 +0530

    Procfile Updated

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 import os
 import environ
 
-API_ID = int(os.environ[29473197])
-API_HASH = os.environ["182c589126cae7e6128a63a5c0fd40d8"]
-BOT_TOKEN = os.environ["843459688:AAGCeQjdFqWGFrucHPRuV4XD-Uuj60WGoBU"]
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
